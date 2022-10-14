01ec125251935d918df04d8aca05519ebfb77a5a
diveragent <cokacollege@gmail.com>
2022-10-14 06:53:01 -0700

    task-1

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+ \** 
+    *main - entry
+   *return: always 0
+  *\ 
+ int main(void)
+ { 
+    printf("with proper grammer , but the out come is a piece of art."); 
+    return (0);
+ }
