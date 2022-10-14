366500e65846ab2ff040384438c390338e3573d5
diveragent <cokacollege@gmail.com>
2022-10-13 17:13:23 -0700

    hello put

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ *main - entry
+ * return: always 0
+ */
+int main (void) {
+puts ("\" programming is like building a multilingual puzzle");
+return (0);
+}
+puts("\" programming is like building a multilingual puzzle");
