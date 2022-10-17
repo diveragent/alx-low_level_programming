7db459f62c0a8c5473552e485ae411da8f652920
diveragent <113716590+diveragent@users.noreply.github.com>
2022-10-17 23:42:29 +0300

    file for task-5

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
+
 /**
- *main - entry
- * return: always 0
- */
-int main (void) {
-puts ("\" programming is like building a multilingual puzzle");
-return (0);
+* main - Entry point
+* 
+* Description: 'the program's description'
+*  
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
-puts("\" programming is like building a multilingual puzzle");
