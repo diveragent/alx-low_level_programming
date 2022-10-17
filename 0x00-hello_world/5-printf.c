6b54c02d80f84961352c9b6029cd6fb1ab8a530b
diveragent <113716590+diveragent@users.noreply.github.com>
2022-10-17 23:47:32 +0300

    file for task-5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
- \** 
-    *main - entry
-   *return: always 0
-  *\ 
- int main(void)
- { 
-    printf("with proper grammer , but the out come is a piece of art."); 
-    return (0);
- }
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
