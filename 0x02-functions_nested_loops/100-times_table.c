5d6a279eb7267469daeeedfb3b52c31b08b840e4
diveragent <113716590+diveragent@users.noreply.github.com>
2022-10-20 04:38:32 +0300

    adv task 0

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,23 +1,51 @@
-#include <stdio.h>
+#include "holberton.h"
 
 /**
- * print_to_98 - prints all natural number from n to 98
- * @n: an integer
- *
- * Return: No return value
+ * print_times_table - prints times table
+ * @n : times table to use
+ * Return:void
  */
-void print_to_98(int n)
-{
-	int step = (n > 98 ? -1 : 1);
-	int i = n;
-	int stop = 98 + step;
 
-	while (i != stop)
+void print_times_table(int n)
+{
+
+int a = 0, rep, b;
+
+if (n < 0 || n > 15)
+	return;
+
+while (a <= n)
+{
+	for (b = 0; b <= n; b++)
 	{
-		if (i != n)
-			printf(", ");
-		printf("%d", i);
-		i += step;
+		rep = a * b;
+		if (b == 0)
+			_putchar('0' + rep);
+		else if (rep < 10)
+		{
+			_putchar(' ');
+			_putchar(' ');
+			_putchar('0' + rep);
+		}
+		else if (rep < 100)
+		{
+			_putchar(' ');
+			_putchar('0' + rep / 10);
+			_putchar('0' + rep % 10);
+		}
+		else
+		{
+			_putchar('0' + rep / 100);
+			_putchar('0' + (rep - 100) / 10);
+			_putchar('0' + rep % 10);
+		}
+		if (b < n)
+		{
+			_putchar(',');
+			_putchar(' ');
+		}
 	}
-	printf("\n");
+	_putchar('\n');
+	a++;
+}
 }
