eedf35971745948924bc66f895752ede7001288b
diveragent <cokacollege@gmail.com>
2022-11-03 00:17:03 -0700

    task 0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -2,16 +2,17 @@
 
 /**
  * * _puts_recursion -> this is a function to print string recursivly
- * * @s: string paramenter
+ * * @: string paramenter
+ * * return: void.
  * */
 void _puts_recursion(char *s)
 {
-		if (*s == '\0')
-				{
-							_putchar('\n');
-									return;
-										}
-			_putchar(*s);
-				s++;
-					_puts_recursion(s);
+	if (*s != '\0')
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	else
+		_putchar('\n');
+
 }
