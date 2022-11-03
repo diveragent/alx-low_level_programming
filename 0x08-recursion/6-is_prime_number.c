34c845b716050983e844f4f17891785dd9b0d434
diveragent <cokacollege@gmail.com>
2022-11-03 00:58:42 -0700

    task 6

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,4 +1,4 @@
-nclude "main.h"
+# include "main.h"
 
 int check_prime(int, int);
 /**
@@ -8,7 +8,14 @@ int check_prime(int, int);
  * */
 int is_prime_number(int n)
 {
-		return (check_prime(n, 1));
+	if (n % a == 0)
+	{
+		if (n == a)
+			return (1);
+		else 
+			return (0);
+	}
+		return (check_prime(n,a + 1));
 }
 
 
@@ -20,11 +27,11 @@ int is_prime_number(int n)
  * */
 int check_prime(int n, int i)
 {
-		if (n <= 1)
-					return (0);
-			if (n % i == 0 && i > 1)
-						return (0);
-				if ((n / i) < i)
-							return (1);
-					return (check_prime(n, i + 1));
+	if (n == 0)
+		return (0);
+	if (n < 0 )
+		return (0);
+	if (n == 1)
+		return (0);
+	return (check_prime(n,2));
 }
