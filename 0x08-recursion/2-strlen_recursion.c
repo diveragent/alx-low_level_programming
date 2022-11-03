a0421be8e78308ee181f10fa8e0feae3667f2b1e
diveragent <cokacollege@gmail.com>
2022-11-03 00:25:47 -0700

    task 2

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -7,8 +7,9 @@
  * */
 int _strlen_recursion(char *s)
 {	
+	/* base condition */
 		if (*s == '\0')
-					return (0);
-			s++;
-				return (1 + _strlen_recursion(s));
+			return (0);
+		else
+			return (1 + _strlen_recursion(s + 1));
 }
