f2b0f65f4ab980dc58297031ee32c84d398fd077
diveragent <cokacollege@gmail.com>
2022-11-03 00:22:06 -0700

    task 1

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -6,10 +6,10 @@
  * */
 void _print_rev_recursion(char *s)
 {
-		if (*s == '\0')
-					return;
-			s++;
-				_print_rev_recursion(s);
-					s--;
-						_putchar(*s);
+		if (*s != '\0')
+		{
+	 		_print_rev_recursion(s + 1);
+			_putchar(*s);
+
+		}
 }
