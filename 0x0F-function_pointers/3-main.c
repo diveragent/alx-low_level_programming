d99ef0ceb33400a1a7995cb415aa23a2e8048540
diveragent <113716590+diveragent@users.noreply.github.com>
2022-11-17 00:20:57 +0300

    main 3

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,71 +1,41 @@
 #include "3-calc.h"
 
 /**
- * op_add - adds two numbers.
- * @a: first number.
- * @b: second number.
+ * main - check the code for Holberton School students.
+ * @argc: argument count.
+ * @argv: argument vector.
  *
- * Return: add.
+ * Return: Always 0.
  */
-int op_add(int a, int b)
+int main(int argc, char *argv[])
 {
-	return (a + b);
-}
+	int a, b;
+	int (*operation)(int, int);
 
-/**
- * op_sub - subctracts two numbers.
- * @a: first number.
- * @b: second number.
- *
- * Return: difference.
- */
-int op_sub(int a, int b)
-{
-	return (a - b);
-}
-
-/**
- * op_mul - multiplies two numbers.
- * @a: first number.
- * @b: second number.
- *
- * Return: multiplication.
- */
-int op_mul(int a, int b)
-{
-	return (a * b);
-}
-
-/**
- * op_div - divides two numbers.
- * @a: first number.
- * @b: second number.
- *
- * Return: division.
- */
-int op_div(int a, int b)
-{
-	if (b == 0)
+	if (argc != 4)
 	{
 		printf("Error\n");
-		exit(100);
+		exit(98);
 	}
-	return (a / b);
-}
 
-/**
- * op_mod - calculates the module of two numbers.
- * @a: first number.
- * @b: second number.
- *
- * Return: remainder of the division.
- */
-int op_mod(int a, int b)
-{
-	if (b == 0)
+	if (argv[2][1])
 	{
 		printf("Error\n");
-		exit(100);
+		exit(99);
 	}
-	return (a % b);
+
+	operation = get_op_func(argv[2]);
+
+	if (operation == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+
+	printf("%d\n", operation(a, b));
+	return (0);
 }
+
