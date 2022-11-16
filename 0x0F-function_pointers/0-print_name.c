4518076c9e4f65593391f4432d7fa32931e36720
diveragent <113716590+diveragent@users.noreply.github.com>
2022-11-17 00:03:54 +0300

    COmmit task 0

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,8 +1,15 @@
-# include "function_positers.h"
+#include "function_pointers.h"
+
 /**
-*/
-void print_name{char *name, void (*f)(char*)}
+ * print_name - prints a name.
+ * @name: input name.
+ * @f: function pointer.
+ *
+ * Return: no return.
+ */
+void print_name(char *name, void (*f)(char *))
 {
-if (name && f)
-  f(name);
+    if (name && f)
+        f(name);
 }
+
